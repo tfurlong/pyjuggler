addc4a1934c7d3c67ce8077a0e5cf758f4d89502
Patrick Hartling <patrick.hartling@gmail.com>
2005-12-22 13:54:01 +0000

    Wrapped long lines. Formatting change only.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/PyJuggler@18323 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- src/snx/_SoundImplementation.cpp
+++ src/snx/_SoundImplementation.cpp
@@ -739,7 +739,8 @@ struct snx_SoundImplementation_Wrapper
       {
          if ( override lookup = this->get_override("lookup") )
          {
-#if defined(_MSC_VER) || (defined(__GNUC__) && __GNUC__ == 3 && __GNUC_MINOR__ == 2)
+#if defined(_MSC_VER) || \
+    (defined(__GNUC__) && __GNUC__ == 3 && __GNUC_MINOR__ == 2)
             return call<snx::SoundInfo&, std::string>(lookup.ptr(),
                                                       boost::ref(p0));
 #else
@@ -792,7 +793,8 @@ struct snx_SoundImplementation_Wrapper
       {
          if ( override name = this->get_override("name") )
          {
-#if defined(_MSC_VER) || (defined(__GNUC__) && __GNUC__ == 3 && __GNUC_MINOR__ == 2)
+#if defined(_MSC_VER) || \
+    (defined(__GNUC__) && __GNUC__ == 3 && __GNUC_MINOR__ == 2)
             return call<std::string&>(name.ptr());
 #else
             return name();
