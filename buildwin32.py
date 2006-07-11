70723f8a89675dc778b716e28c2c7a9241c71e08
Patrick Hartling <patrick.hartling@gmail.com>
2006-07-11 13:51:39 +0000

    MFT [rev 18981]: Add code to install header files on windows.
    git-svn-id: https://vrjuggler.googlecode.com/svn/PyJuggler/branches/1.0@19009 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- buildwin32.py
+++ buildwin32.py
@@ -92,6 +92,7 @@ def doInstall(prefix):
    installDist(prefix)
    installLibs(prefix)
    installMods(prefix)
+   installHeaders(prefix)
    installExamples(prefix)
 
 def mkinstalldirs(dir):
@@ -105,6 +106,7 @@ def makeTree(prefix):
    mkinstalldirs(os.path.join(prefix, 'lib'))
    mkinstalldirs(os.path.join(prefix, python_pkg_dir))
    mkinstalldirs(os.path.join(prefix, 'share', 'pyjuggler'))
+   mkinstalldirs(os.path.join(prefix, 'include'))
 
 def installDir(startDir, destDir, allowedExts = None, disallowedExts = None,
                disallowedFiles = None):
@@ -189,6 +191,12 @@ def installMods(prefix):
    mkinstalldirs(destdir)
    for f in files:
       shutil.copy2(f, destdir)
+
+def installHeaders(prefix):
+   print "Installing header files ..."
+   destdir = os.path.join(prefix, 'include', 'pyjutil')
+   srcdir  = os.path.join(pyj_dir, 'pyjutil')
+   installDir(srcdir, destdir, allowedExts = [".h",])
 
 def installExamples(prefix):
    print "Installing example code ..."
