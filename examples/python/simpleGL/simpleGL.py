776327d4fb7043ca8538de799c5e7180187dfa82
Patrick Hartling <patrick.hartling@gmail.com>
2010-01-02 16:10:59 +0000

    Copyright update.
    git-svn-id: https://vrjuggler.googlecode.com/svn/PyJuggler/trunk@21186 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- examples/python/simpleGL/simpleGL.py
+++ examples/python/simpleGL/simpleGL.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# PyJuggler is (C) Copyright 2002-2009 by Patrick Hartling
+# PyJuggler is (C) Copyright 2002-2010 by Patrick Hartling
 # Distributed under the GNU Lesser General Public License 2.1.  (See
 # accompanying file COPYING.txt or http://www.gnu.org/copyleft/lesser.txt)
 
