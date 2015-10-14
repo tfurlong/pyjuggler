2dce0d4518ed3dbfa45ed41a4f76a341a1fa5f4d
Casey Megginson <tenncasey@gmail.com>
2015-10-14 14:37:28 -0500

    Added comments explaining how the initialization scripts are meant to be used.

--- examples/python/simpleGL/execute.bat
+++ examples/python/simpleGL/execute.bat
@@ -1,8 +1,16 @@
 @echo off
 setlocal
 
+REM Set the PATH variable to include any directories with custom libraries that
+REM you're using.
 set PATH=%PATH%;D:\libs\boost_1_55_0\lib64-msvc-12.0;D:\libs\vrjuggler\bin;D:\libs\xml-cppdom\bin;D:\libs\freealut\bin;D:\libs\openal\bin;D:\programs\Python27\DLLs;D:\pythonlibs\PyJuggler
+
+REM Set the PYTHONPATH variable to point to where you have installed any custom
+REM python modules such as PyJuggler itself.
 set PYTHONPATH=D:\pythonlibs;D:\libs\boost_1_55_0\lib64-msvc-12.0;D:\libs\vrjuggler\bin;D:\libs\xml-cppdom\bin;D:\programs\Python27\DLLs
+
+REM Run your application with an argument pointing to the VRJuggler config
+REM file you would like to use.
 python  simpleGL.py S:\tools\vrjuggler-config\desktop\desktop-cavesim.jconf
 
 endlocal
--- examples/python/simpleGL/execute.sh
+++ examples/python/simpleGL/execute.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+# If PYTHONPATH hasn't been set, you'll need to set it here to point to where
+# PyJuggler is located.
+# export PYTHONPATH=/path/to/python/libs
 
-#export VOGL_CMD_LINE="--vogl_tracefile vogltrace.simpleGL.bin"
+# Set the library path so that any required libraries can be found at runtime.
+# Note that the path to PyJuggler itself has to be there due to pyjutil.
 export LD_LIBRARY_PATH=~/usr/lib64:~/usr/pythonlibs/PyJuggler
-#export LD_PRELOAD=$(readlink -f ~/usr/vogl/libvogltrace64.so)
+
+# Run your application with an argument pointing to the VRJuggler config
+# file you would like to use.
 ./simpleGL.py ~/smb-shared/tools/vrjuggler-config/desktop/desktop-cavesim.jconf
